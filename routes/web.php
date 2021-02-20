c559d061863fc0095bf930a97511fc8c2a92cab7
Samuel <jos90500@gmail.com>
2021-02-19 23:27:17 -0600

    web
    configuracion de enlaces

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@posts');
+Route::get('blog/{post}', 'PageController@post')->name('post');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
