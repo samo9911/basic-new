b8d8dd57febf72c228575c6058e48da1ece1989f
Samuel <jos90500@gmail.com>
2021-02-19 23:55:26 -0600

    PageController
    Controladores de nuestra pagina

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function posts(){
+    return view('posts', [
+        'posts' => Post::load('users')->latest()->paginate()
+    ]);
+    }
+    public function post(Post $post){
+        return view('post', ['post' => $post]);
+    }
+}
